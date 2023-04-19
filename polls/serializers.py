2c25df25bbef3b27676a4e649328079f34b0b778
Rabiyulfahim <76527084+rabiyulfahimhasim786@users.noreply.github.com>
2023-04-19 08:31:31 -0500

    Add files via upload

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
-from .models import Pageperformance, MobilePageperformance
+from .models import Pageperformance
 
 class PageperformanceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Pageperformance
         fields = ('id', 'input_url', 'output_data')
-
-
-class MobileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MobilePageperformance
-        fields = ('id', 'mobile_url', 'mobile_data')
